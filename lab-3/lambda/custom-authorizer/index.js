136e2f50149402638b87d2f648e487d3392854ca
sauravraghuvanshi <sauravraghuvanshi24@gmail.com>
2020-08-22 11:24:45 +0530

    Adding Lab 3

---
+++ lab-3/lambda/custom-authorizer/index.js
@@ -0,0 +1,110 @@
+'use strict';
+
+/**
+ *
+ * Required Env consts:
+ * AUTH0_DOMAIN
+ */
+
+const jwt = require('jsonwebtoken');
+const rp = require('request-promise');
+
+
+const generatePolicy = (principalId, effect, resource) => {
+    const authResponse = {};
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        const statementOne = {
+            Action: 'execute-api:Invoke', // default action
+            Effect: effect,
+            Resource: resource,
+        }
+        const policyDocument = {
+            Version: '2012-10-17',
+            Statement: [statementOne]
+        }
+        authResponse.policyDocument = policyDocument;
+    }
+    return authResponse;
+};
+
+
+const verifyJWTToken = (jwtToken, pubKey) => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(jwtToken, pubKey, { algorithms: ['RS256'] }, (err, decoded) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(decoded);
+            }
+        });
+    });
+};
+
+const handler = (event, context, callback) => {
+    
+    // The authorization token is in the form of "Authentication": "Bearer (some ID token from the frontent)"
+    if (!event.authorizationToken) {
+    	callback('Could not find authToken');
+    	return;
+    }
+
+    const jwtToken = event.authorizationToken.split(' ')[1];
+    if (!jwtToken) {
+        callback('Could not find authToken');
+    	return;
+    }
+    const decodedToken = jwt.decode(jwtToken, {complete: true});
+
+    const Auth0ApiBaseUrl = process.env.AUTH0_DOMAIN;
+    if(!Auth0ApiBaseUrl) {
+        callback('Base Url not found');
+    	return;
+    }
+
+    return rp(`https://${Auth0ApiBaseUrl}/.well-known/jwks.json`)
+        .then((jwks) => {
+
+            const jwksKey = JSON.parse(jwks).keys[0];
+
+            //Validate the algorithm
+            if (!jwksKey) {
+                throw new Error('No supported jwt keys');
+             }
+
+            //Validate the algorithm
+            if (jwksKey.alg !== 'RS256' || decodedToken.header.alg !== 'RS256') {
+               throw new Error('Invalid algorithm used, only RS256 supported');
+            }
+
+            //Validate the signing key
+            if (!jwksKey.kid || decodedToken.header.kid !== jwksKey.kid) {
+                throw new Error('Invalid signing algorithm');
+            }
+
+            //Validate the certificate
+            if (!jwksKey.x5c[0]) {
+                throw new Error('No certificate found');
+            }
+
+            const cert = `-----BEGIN CERTIFICATE-----\n${jwksKey.x5c[0]}\n-----END CERTIFICATE-----\n`;
+
+            return cert;
+
+        })
+        .then((pubKey) => verifyJWTToken(jwtToken, pubKey))
+        .then(() => {
+            callback(null, generatePolicy('user', 'allow', event.methodArn));
+        })
+        .catch((err) => {
+
+            console.log('Failed jwt verification: ', err, 'auth: ', event.authorizationToken);
+            callback('Authorization Failed');
+
+        });
+
+};
+
+module.exports = {
+    handler
+};
---
+++ lab-3/lambda/user-profile/index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+/**
+ *
+ * Required Env Vars:
+ * AUTH0_DOMAIN
+ */
+
+const rp = require('request-promise');
+
+const generateResponse = (status, message) => {
+    return {
+        statusCode: status,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify({'message':message})
+    }
+};
+
+const handler = (event, context, callback) => {
+
+    console.log(JSON.stringify(event, null, 2));
+
+    // Grab the Id token from the request header, the access token from the url
+    // and the auth0 doimain name from the environment variables
+    const accessToken = event.queryStringParameters.accessToken;
+    const auth0Domain = process.env.AUTH0_DOMAIN;
+
+    // Check to make sure we have the access token
+    if (!accessToken) {
+        const response = generateResponse(400, 'AccessToken not found');
+  
+        callback(null, response);
+        return;
+    }
+
+
+    // Build and make a request to Auth0's userinfo endpoint
+    // to get information on the user from the access token
+    const options = {
+        url: `https://${auth0Domain}/userinfo`,
+        method: 'POST',
+        json: true,
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    };
+
+    return rp(options).then((body) => {
+
+            // Success
+            const response = generateResponse(200, body);
+            callback(null, response);
+
+        })
+        .catch((error) => {
+
+            // Failure
+            const response = generateResponse(400, error);
+            callback(null, response);
+
+        });
+};
+
+module.exports = {
+    handler
+};
